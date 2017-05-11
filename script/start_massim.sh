81a3891dba560da2edb14078c6fc588ff0493a4c
Christopher Hrabia <c.hrabia@gmail.com>
2017-05-11 14:29:52 +0200

    updated start script to VM structure

--- script/start_massim.sh
+++ script/start_massim.sh
@@ -5,7 +5,7 @@ VERSION_DIRECTORY=2017-1.1
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd "$SCRIPT_DIRECTORY/../massim-$VERSION_DIRECTORY/server"
+cd "$SCRIPT_DIRECTORY/../../massim/massim-$VERSION_DIRECTORY/server"
 
 java -jar "server-$VERSION_JAR-jar-with-dependencies.jar" --monitor
 
