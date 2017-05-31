85ec5ea9b7e4310126f16de6a59a0b898a72d563
Christopher Hrabia <c.hrabia@gmail.com>
2017-05-31 11:49:36 +0200

    updated to release 1.2

--- script/start_massim.sh
+++ script/start_massim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION_JAR=2017-0.2
-VERSION_DIRECTORY=2017-1.1
+VERSION_JAR=2017-0.3
+VERSION_DIRECTORY=2017-1.2
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -9,4 +9,4 @@ cd "$SCRIPT_DIRECTORY/../../massim/massim-$VERSION_DIRECTORY/server"
 
 java -jar "server-$VERSION_JAR-jar-with-dependencies.jar" --monitor
 
-#Monitor available at http://localhost:8000/ 
+#Monitor available at http://localhost:8000/
