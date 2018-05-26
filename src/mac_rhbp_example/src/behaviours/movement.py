453449edc95bbae4a35cf4e4b460c65574b1e644
etti <michael@ettlinger.me>
2018-05-26 12:13:36 +0100

    Refactored movement: use static methods, use centralized Knowledgebase

--- src/mac_rhbp_example/src/behaviours/movement.py
+++ src/mac_rhbp_example/src/behaviours/movement.py
@@ -16,47 +16,15 @@ from behaviour_components.behaviours import BehaviourBase
 from behaviours.generic_action import GenericActionBehaviour, Action
 
 
-def action_goto_location(lat,long, publisher):
-    """
-    Specific "goto" action publishing helper function
-    :param facility_name: name of the facility we want to go to
-    :param publisher: publisher to use
-    """
-    action = GenericAction()
-    action.action_type = Action.GO_TO
-    action.params = [
-        KeyValue("latitude", str(lat)),
-        KeyValue("longitude", str(long))]
-    publisher.publish(action)
 
-
-def action_continue(publisher):
-    """
-    Specific "continue" action publishing helper function
-    :param publisher: publisher to use
-    """
-    action_type = "continue"
-
-    action_generic(publisher, action_type)
-
-
-def action_abort(publisher):
-    """
-    Specific "abort" action publishing helper function
-    :param publisher: publisher to use
-    """
-    action_type = "abort"
-
-    action_generic(publisher, action_type)
-
-class GotoFacilityBehaviour(BehaviourBase):
+class GotoLocationBehaviour(BehaviourBase):
     """
     Behaviour that explores the environment by going to a randomly selected facility
     """
 
     def __init__(self, agent_name, facility_topic, **kwargs):
 
-        super(GotoFacilityBehaviour, self) \
+        super(GotoLocationBehaviour, self) \
             .__init__(requires_execution_steps=True, **kwargs)
 
         self._agent_name = agent_name
@@ -72,16 +40,46 @@ class GotoFacilityBehaviour(BehaviourBase):
         self._pub_generic_action = rospy.Publisher(AgentUtils.get_bridge_topic_prefix(agent_name) + 'generic_action', GenericAction
                                                    , queue_size=10)
 
-        self._exploration_knowledge = MovementKnowledge.get_knowledge_base_tuple_facility_exploration(self._agent_name,
-                                                                                    self._facility_topic)
+    @staticmethod
+    def action_goto_location(lat, long, publisher):
+        """
+        Specific "goto" action publishing helper function
+        :param facility_name: name of the facility we want to go to
+        :param publisher: publisher to use
+        """
+        action = GenericAction()
+        action.action_type = Action.GO_TO
+        action.params = [
+            KeyValue("latitude", str(lat)),
+            KeyValue("longitude", str(long))]
+        publisher.publish(action)
 
+    @staticmethod
+    def action_continue(publisher):
+        """
+        Specific "continue" action publishing helper function
+        :param publisher: publisher to use
+        """
+        action_type = "continue"
+
+        GenericActionBehaviour.action_generic(publisher, action_type)
+
+    @staticmethod
+    def action_abort(publisher):
+        """
+        Specific "abort" action publishing helper function
+        :param publisher: publisher to use
+        """
+        action_type = "abort"
+
+        GenericActionBehaviour.action_generic(publisher, action_type)
 
     def move(self):
 
         if self._selected_pos: # in case we did not find/know a facility
 
             rospy.loginfo(self._agent_name + "::" +self._name + " to "+ str(self._selected_pos))
-            action_goto_location(lat=self._selected_pos.lat, long=self._selected_pos.long, publisher=self._pub_generic_action)
+            GotoLocationBehaviour.action_goto_location(lat=self._selected_pos.lat, long=self._selected_pos.long, publisher=self._pub_generic_action)
         else: # backup action recharge agent
             rospy.loginfo(self._agent_name + "::" + self._name + " recharging because of missing facility.")
             GenericActionBehaviour.action_generic_simple(publisher=self._pub_generic_action,action_type='recharge')
@@ -99,7 +97,6 @@ class GotoFacilityBehaviour(BehaviourBase):
         rospy.loginfo(self._agent_name + "::" + self._name + " selected facility: " + str(self._selected_pos))
 
         # we set to false in order to fulfil the requirements of FinishExplorationBehaviour
-        self.__client.update(self._exploration_knowledge + ('*',), self._exploration_knowledge + ('false',), )
 
         if self._selected_pos:
             self._movement_knowledge.start_movement(self._selected_pos)
@@ -110,7 +107,7 @@ class GotoFacilityBehaviour(BehaviourBase):
 
         rospy.loginfo(self._agent_name + "::" + self._name + " disabled")
 
-        super(GotoFacilityBehaviour, self).stop()
+        super(GotoLocationBehaviour, self).stop()
 
     def do_step(self):
 
