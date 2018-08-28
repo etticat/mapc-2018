a003b40ab45053400b035d10d15c198f4e16468b
etti <michael@ettlinger.me>
2018-08-28 14:45:32 +0100

    Simplified gathering network behaviour

--- src/mapc_rhbp_ettlinger/src/network_behaviours/gather.py
+++ src/mapc_rhbp_ettlinger/src/network_behaviours/gather.py
@@ -6,6 +6,7 @@ from behaviour_components.goals import GoalBase
 from behaviours.generic_action import GenericActionBehaviour
 from behaviours.movement import GoToDestinationBehaviour
 from network_behaviours.battery import BatteryChargingNetworkBehaviour
+from network_behaviours.go_and_do import GoAndDoNetworkBehaviour
 from provider.action_provider import Action
 from provider.product_provider import ProductProvider
 from rhbp_selforga.gradientsensor import GradientSensor, SENSOR
@@ -13,51 +14,28 @@ from sensor.general import SubtractionSensor
 from sensor.movement import StepDistanceSensor
 
 
-class GatheringNetworkBehaviour(BatteryChargingNetworkBehaviour):
+class GatheringNetworkBehaviour(GoAndDoNetworkBehaviour):
     """
     Network behaviour responsible for gathering ingredients
-    TODO: This could also inherit from GoAndDoBehaviour to safe a little bit of code
     """
 
     def __init__(self, agent_name, name, global_rhbp_components, **kwargs):
-        super(GatheringNetworkBehaviour, self).__init__(name=name, agent_name=agent_name, global_rhbp_components=global_rhbp_components,
-                                                         **kwargs)
+
+        super(GatheringNetworkBehaviour, self).__init__(
+            agent_name=agent_name,
+            global_rhbp_components=global_rhbp_components,
+            name=name,
+            mechanism=global_rhbp_components.gather_decision_mechanism,
+            **kwargs)
 
         self._agent_name = self._agent_name
 
-        self._product_provider = ProductProvider(agent_name=self._agent_name)
-
-        self.init_sensors(global_rhbp_components=global_rhbp_components)
-        self.init_go_to_resource_behaviour(global_rhbp_components)
-        self.init_gather_behaviour()
-
-        self.apply_charging_restrictions(self.go_to_resource_node_behaviour)
-
-        self.charge_goal = GoalBase(
-            name='gather_goal',
-            permanent=True,
-            priority=100,
-            planner_prefix=self.get_manager_prefix(),
-            conditions=[self._global_rhbp_components.load_fullness_condition])
-
-    def init_gather_behaviour(self):
-        """
-        Initialise gather behaviour
-        :return:
-        """
-        ############### Gathering ##########################
         self.gather_behviour = GenericActionBehaviour(
             name="gather_behaviour",
             action_type=Action.GATHER,
             agent_name=self._agent_name,
             plannerPrefix=self.get_manager_prefix()
         )
-        # Only gather if we are at the intended resource node
-        self.gather_behviour.add_precondition(
-            precondition=self.at_resource_node_condition)
-
-        self.gather_behviour.add_precondition(
-            precondition=self.next_item_fits_in_storage_cond)
 
         self.gather_behviour.add_effect(
             effect=Effect(
@@ -68,77 +46,14 @@ class GatheringNetworkBehaviour(BatteryChargingNetworkBehaviour):
             )
         )
 
-    def init_go_to_resource_behaviour(self, global_rhbp_components):
-        """
-        Initialise movement behaviour
-        :param global_rhbp_components:
-        :return:
-        """
-        ################ Going to resource #########################
-        self.go_to_resource_node_behaviour = GoToDestinationBehaviour(
-            agent_name=self._agent_name,
-            name="go_to_resource_node_behaviour",
-            plannerPrefix=self.get_manager_prefix(),
-            mechanism=global_rhbp_components.gather_decision_mechanism
-        )
+        self.init_do_behaviour(self.gather_behviour)
 
-        self.gather_target_sensor = GradientSensor(
-            mechanism=self._global_rhbp_components.gather_decision_mechanism,
-            name="gather_target_sensor",
-            sensor_type=SENSOR.VALUE
-        )
-        # Sensor to check distance to charging station
-        self.target_step_sensor = StepDistanceSensor(
-            name='gather_target_step_sensor',
-            agent_name=self._agent_name,
-            destination_sensor=self.gather_target_sensor,
-            initial_value=10
-        )
-        self.at_resource_node_condition = Condition(
-            sensor=self.target_step_sensor,
-            activator=ThresholdActivator(
-                thresholdValue=0,
-                isMinimum=False))  # highest activation if the value is below threshold
-
-        # Only go to resource node if we aren't already there
-        self.go_to_resource_node_behaviour.add_precondition(
-            precondition=Negation(self.at_resource_node_condition))
-
-        # Going to resource has the effect of decreasing the distance to go there
-        self.go_to_resource_node_behaviour.add_effect(
-            effect=Effect(
-                sensor_name=self.target_step_sensor.name,
-                indicator=-1.0,  # decreasing by 1 step
-                sensor_type=float
-
-            )
-        )
-
-    def init_sensors(self, global_rhbp_components):
-        """
-        Initialise sensors
-        :param global_rhbp_components:
-        :return:
-        """
-
-        self.next_ingredient_volume_sensor = GradientSensor(
-            mechanism=global_rhbp_components.gather_decision_mechanism,
-            sensor_type=SENSOR.VALUE_ATTRIBUTE,
-            attr_name="item.volume",
-            initial_value=0,
-            name="next_ingredient_volume_sensor"
-        )
-
-        self.load_after_next_ingredient_sensor = SubtractionSensor(
-            minuend_sensor=self._global_rhbp_components.free_load_sensor,
-            subtrahend_sensor=self.next_ingredient_volume_sensor,
-            name="load_after_next_ingredient_sensor"
-        )
-        self.next_item_fits_in_storage_cond = Condition(
-            sensor=self.load_after_next_ingredient_sensor,
-            activator=ThresholdActivator(thresholdValue=0, isMinimum=True)
-        )
-
+        self.gather_goal = GoalBase(
+            name='gather_goal',
+            permanent=True,
+            priority=50,
+            planner_prefix=self._agent_name,
+            conditions=[self._global_rhbp_components.load_fullness_condition])
 
     def stop(self):
         """
